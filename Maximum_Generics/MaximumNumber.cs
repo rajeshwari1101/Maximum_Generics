466be8a0a951131d44d6c748d1de3135e342c35c
rajeshwari1101 <rajeshwaripatil1101@gmail.com>
2022-02-17 22:44:35 +0530

    Maximum method can take any number of parameters

--- Maximum_Generics/MaximumNumber.cs
+++ Maximum_Generics/MaximumNumber.cs
@@ -1,37 +1,16 @@
 ﻿namespace Maximum_Generics;
 
-internal static class MaximumNumber 
+internal class MaximumNumber<T> where T : IComparable
 {
-    // Finds Max of 3 integers
-    public static void IntMax(int num1, int num2, int num3)
+    private T[] array;
+    public MaximumNumber(T[] array)
     {
-        if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) >= 0)
-            Console.WriteLine($"Max value is: {num1}");
-        else if (num2.CompareTo(num3) >= 0)
-            Console.WriteLine($"Max value is: {num2}");
-        else
-            Console.WriteLine($"Max value is: {num3}");
+        this.array = array;
     }
-
-    /// Finds Max of 3 floats
-    public static void FloatMax(float num1, float num2, float num3)
+    public void Max(params T[] array)
     {
-        if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) >= 0)
-            Console.WriteLine($"Max value is: {num1}");
-        else if (num2.CompareTo(num3) >= 0)
-            Console.WriteLine($"Max value is: {num2}");
-        else
-            Console.WriteLine($"Max value is: {num3}");
-    }
-
-    /// Finds Max of 3 strings
-    public static void StringMax(string str1, string str2, string str3)
-    {
-        if (str1.CompareTo(str2) >= 0 && str1.CompareTo(str3) >= 0)
-            Console.WriteLine($"Max value is: {str1}");
-        else if (str2.CompareTo(str3) >= 0)
-            Console.WriteLine($"Max value is: {str2}");
-        else
-            Console.WriteLine($"Max value is: {str3}");
+        this.array = array;
+        Array.Sort(array);
+        Console.WriteLine($"Max value is: {array[2]}");
     }
 }
--- Maximum_Generics/TestCase.cs
+++ Maximum_Generics/TestCase.cs
@@ -2,6 +2,7 @@
 
 internal static class TestCase
 {
+    // Test case array attributes
     private static readonly int[] intTestCase1;
     private static readonly int[] intTestCase2;
     private static readonly int[] intTestCase3;
@@ -12,8 +13,12 @@ internal static class TestCase
     private static readonly string[] stringTestCase2;
     private static readonly string[] stringTestCase3;
 
+    // Generic class objects
+    private static MaximumNumber<int> maxInt;
+    private static MaximumNumber<float> maxFloat;
+    private static MaximumNumber<string> maxString;
+
     // Initializes the <see cref="TestCase"/> class.
-  
     static TestCase()
     {
         intTestCase1 = new int[] { 5, 3, 9 };
@@ -25,6 +30,9 @@ internal static class TestCase
         stringTestCase1 = new string[] { "Apple", "Banana", "Chocolate" };
         stringTestCase2 = new string[] { "Choco", "Chocolate", "Banana" };
         stringTestCase3 = new string[] { "Zoom", "Zap", "Zip" };
+        maxInt = new(intTestCase1);
+        maxFloat = new(floatTestCase1);
+        maxString = new(stringTestCase1);
     }
 
     // returns the specified test case as string.
@@ -34,37 +42,36 @@ internal static class TestCase
         return $"[{testCase[0]}, {testCase[1]}, {testCase[2]}]";
     }
 
-    // Tests the max function for 3 int.
+    /// Tests the max function for 3 int.
     public static void IntTest()
     {
         Console.WriteLine("Test case 1: " + GetString(intTestCase1));
-        MaximumNumber.IntMax(intTestCase1[0], intTestCase1[1], intTestCase1[2]);
+        maxInt.Max(intTestCase1[0], intTestCase1[1], intTestCase1[2]);
         Console.WriteLine("Test case 2: " + GetString(intTestCase2));
-        MaximumNumber.IntMax(intTestCase2[0], intTestCase2[1], intTestCase2[2]);
+        maxInt.Max(intTestCase2[0], intTestCase2[1], intTestCase2[2]);
         Console.WriteLine("Test case 3: " + GetString(intTestCase3));
-        MaximumNumber.IntMax(intTestCase3[0], intTestCase3[1], intTestCase3[2]);
+        maxInt.Max(intTestCase3[0], intTestCase3[1], intTestCase3[2]);
     }
 
-    // Tests the max function for 3 float.
-
+    /// Tests the max function for 3 float.
     public static void FloatTest()
     {
         Console.WriteLine("Test case 1: " + GetString(floatTestCase1));
-        MaximumNumber.FloatMax(floatTestCase1[0], floatTestCase1[1], floatTestCase1[2]);
+        maxFloat.Max(floatTestCase1[0], floatTestCase1[1], floatTestCase1[2]);
         Console.WriteLine("Test case 2: " + GetString(floatTestCase2));
-        MaximumNumber.FloatMax(floatTestCase2[0], floatTestCase2[1], floatTestCase2[2]);
+        maxFloat.Max(floatTestCase2[0], floatTestCase2[1], floatTestCase2[2]);
         Console.WriteLine("Test case 3: " + GetString(floatTestCase3));
-        MaximumNumber.FloatMax(floatTestCase3[0], floatTestCase3[1], floatTestCase3[2]);
+        maxFloat.Max(floatTestCase3[0], floatTestCase3[1], floatTestCase3[2]);
     }
 
     /// Tests the max function for 3 strings
     public static void StringTest()
     {
         Console.WriteLine("Test case 1: " + GetString(stringTestCase1));
-        MaximumNumber.StringMax(stringTestCase1[0], stringTestCase1[1], stringTestCase1[2]);
+        maxString.Max(stringTestCase1[0], stringTestCase1[1], stringTestCase1[2]);
         Console.WriteLine("Test case 2: " + GetString(stringTestCase2));
-        MaximumNumber.StringMax(stringTestCase2[0], stringTestCase2[1], stringTestCase2[2]);
+        maxString.Max(stringTestCase2[0], stringTestCase2[1], stringTestCase2[2]);
         Console.WriteLine("Test case 3: " + GetString(stringTestCase3));
-        MaximumNumber.StringMax(stringTestCase3[0], stringTestCase3[1], stringTestCase3[2]);
+        maxString.Max(stringTestCase3[0], stringTestCase3[1], stringTestCase3[2]);
     }
 }
