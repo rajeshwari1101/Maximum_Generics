1b3beb370307583119efc26957209daf38206ad2
rajeshwari1101 <rajeshwaripatil1101@gmail.com>
2022-02-17 21:25:58 +0530

    Maximum of 3 Integers

---
+++ Maximum_Generics/MaximumNumber.cs
@@ -0,0 +1,14 @@
+﻿namespace Maximum_Generics;
+ 
+internal class MaximumNumber 
+{
+    public static void FindIntMax(int num1, int num2, int num3)
+    {
+        if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) >= 0)
+            Console.WriteLine($"Max value is: {num1}");
+        else if (num2.CompareTo(num3) >= 0)
+            Console.WriteLine($"Max value is: {num2}");
+        else
+            Console.WriteLine($"Max value is: {num3}");
+    }
+}
